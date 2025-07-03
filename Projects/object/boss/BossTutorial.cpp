06a12cde1d5ceab4f0b36a6953343dd772683f51
4zu1Q <Sota710794@i.softbank.jp>
2025-07-03 11:40:25 +0900

    up

--- Projects/object/boss/BossTutorial.cpp
+++ Projects/object/boss/BossTutorial.cpp
@@ -863,10 +863,9 @@ void BossTutorial::OnHitOneDamage()
 	auto pos = m_rigidbody.GetPos();
 	EffectManager::GetInstance().CreateEffect("bossHitEffect", VGet(pos.x, pos.y + 15.0f, pos.z));
 	m_pAnim->ChangeAnim(kAnimCoolTime);
-	m_updateFunc = &BossTutorial::HitOneDamageUpdate;
 
 	//攻撃判定がバグらなければこっちの方がボスの難易度が上がってよい
-	//m_updateFunc = &BossTutorial::IdleUpdate;
+	m_updateFunc = &BossTutorial::IdleUpdate;
 }
 
 void BossTutorial::OnHitTwoDamage()
@@ -905,10 +904,9 @@ void BossTutorial::OnHitTwoDamage()
 	auto pos = m_rigidbody.GetPos();
 	EffectManager::GetInstance().CreateEffect("bossHitEffect", VGet(pos.x, pos.y + 15.0f, pos.z));
 	m_pAnim->ChangeAnim(kAnimCoolTime);
-	m_updateFunc = &BossTutorial::HitTwoDamageUpdate;
 
 	//攻撃判定がバグらなければこっちの方がボスの難易度が上がってよい
-	//m_updateFunc = &BossTutorial::IdleUpdate;
+	m_updateFunc = &BossTutorial::IdleUpdate;
 }
 
 void BossTutorial::OnDown()
